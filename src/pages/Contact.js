6612726fdc7330524aa63f1e5dc51731c6e36909
AhmadBuba <ahmadbubacherryfield@gmail.com>
2022-11-06 00:04:14 +0100

    feat: route readded

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,74 +1,11 @@
-import { useState } from "react";
 import classes from "./Contact.module.css";
 
 const Contact = () => {
-  const [enteredFirstName, setEnteredFirstName] = useState("");
-  const [enteredFirstNameTouched, setEnteredFirstNameTouched] = useState(false);
-  const [enteredLastName, setEnteredLastName] = useState("");
-  const [enteredLastNameTouched, setEnteredLastNameTouched] = useState(false);
-  const [enteredMail, setEnteredMail] = useState("");
-  const [enteredMailTouched, setEnteredMailTouched] = useState(false);
-  const [enteredMessage, setEnteredMessage] = useState("");
-  const [enteredMessageTouched, setEnteredMessageTouched] = useState(false);
-  const [enteredCheckbox, setEnteredCheckbox] = useState(false);
-  const [enteredCheckboxTouched, setEnteredCheckboxTouched] = useState(false);
-  const [enteredForm, setEnteredForm] = useState(false);
-
-  const firstNameChangeHandler = (event) => {
-    setEnteredFirstName(event.target.value);
-  };
-
-  // const firstNameValid = enteredFirstName.trim().length > 0 ? true : false;
-
-  const lastNameChangeHandler = (event) => {
-    setEnteredLastName(event.target.value);
-  };
-
-  // const lastNameValid = enteredLastName.trim().length > 0 ? true : false;
-
-  const mailChangeHandler = (event) => {
-    setEnteredMail(event.target.value);
-  };
-
-  // const mailValid =
-  //   enteredMail.trim().length > 0 && enteredMail.includes("@") ? true : false;
-
-  const messageChangeHandler = (event) => {
-    setEnteredMessage(event.target.value);
-  };
-
-  // const messageValid = enteredMessage.trim().length > 0 ? true : false;
-
-  const checkBoxChangeHandler = (event) => {
-    setEnteredCheckbox((prevValue) => !prevValue);
-  };
-
-  // const checkBoxValid = enteredCheckbox ? true : false;
-
-  const submitHandler = (event) => {
-    event.preventDefault();
-
-    const userData = {
-      first_name: enteredFirstName,
-      last_name: enteredLastName,
-      email: enteredMail,
-      message: enteredMessage,
-    };
-
-    console.log(userData.first_name);
-
-    setEnteredFirstName("");
-    setEnteredLastName("");
-    setEnteredMail("");
-    setEnteredMessage("");
-    setEnteredCheckbox(false);
-  };
-
   return (
     <div className={classes.holder}>
       <h2>Contact Me</h2>
       <p>Hi there, contact me to ask me about anything you have in mind.</p>
-      <form onSubmit={submitHandler}>
+      <form>
         <div className={classes.flex_display}>
           <div className={classes.block_display_name}>
             <label htmlFor="first_name">First name</label>
@@ -77,8 +14,6 @@ const Contact = () => {
               name="name"
               id="first_name"
               placeholder="Enter your first name"
-              value={enteredFirstName}
-              onChange={firstNameChangeHandler}
             />
           </div>
 
@@ -89,8 +24,6 @@ const Contact = () => {
               name="name"
               id="last_name"
               placeholder="Enter your last name"
-              value={enteredLastName}
-              onChange={lastNameChangeHandler}
             />
           </div>
         </div>
@@ -100,8 +33,6 @@ const Contact = () => {
           name="email"
           id="email"
           placeholder="yourname@email.com"
-          value={enteredMail}
-          onChange={mailChangeHandler}
           className={classes.block_display}
         />
 
@@ -112,20 +43,12 @@ const Contact = () => {
           maxLength="400"
           id="message"
           placeholder="Send me a message and I'll reply you as soon as possible "
-          value={enteredMessage}
-          onChange={messageChangeHandler}
           className={`${classes.block_display} ${classes.pad}`}
           // onClick={onFocusHandler}
         />
 
         <div>
-          <input
-            type="checkbox"
-            name="agreement"
-            onClick={checkBoxChangeHandler}
-            value={enteredCheckbox}
-            className={classes.check}
-          />
+          <input type="checkbox" name="agreement" className={classes.check} />
           <label htmlFor="agreement">
             You agree to providing your data to Ahmad who may contact you.
           </label>
--- src/pages/components/Profile/Profile.js
+++ src/pages/components/Profile/Profile.js
@@ -1,10 +1,7 @@
 import React from "react";
 
 import classes from "./Profile.module.css";
-import profilePic from "../../../assests/profilePic.JPG";
-import cameraIcon from "../../../assests/Icon.png";
-import arrowIcon from "../../../assests/Content.png";
-import dotsIcon from "../../../assests/Contentt.png";
+
 import Icon from "./Icon";
 import ProfilePhoto from "./ProfilePhoto";
 
